54ea3efaa04f2c0d8000e9a9b28184de0ebb5e1e
Christopher Demicoli <christopher.demicoli@outlook.com>
2019-03-10 20:04:56 +0100

    Reading all Data from ADSB Hub

---
+++ Runner/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace Runner
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            using (TcpClient adsbhubClient = new TcpClient("data.adsbhub.org", 5002))
+            using (NetworkStream adsbNetworkStream = adsbhubClient.GetStream())
+            using (StreamReader adsbStreamReader = new StreamReader(adsbNetworkStream))
+            {
+                processMessage(await adsbStreamReader.ReadLineAsync());
+                while (!adsbStreamReader.EndOfStream)
+                {
+                    processMessage(await adsbStreamReader.ReadLineAsync());
+                }
+            }
+        }
+
+        private static void processMessage(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
